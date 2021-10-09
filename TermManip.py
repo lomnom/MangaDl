67fb1379bdec1fae855272df5257c2edb60f0a22
lomnom <zhaoxiong.ang@gmail.com>
2021-10-09 11:25:07 +0800

    comment node

--- TermManip.py
+++ TermManip.py
@@ -1,4 +1,4 @@
-black="\033[30m"
+black="\033[30m" #escape codes to change terminal fg color
 red="\033[31m"
 green="\033[32m"
 yellow="\033[33m"
@@ -9,56 +9,54 @@ white="\033[37m"
 default="\033[39m"
 reset="\033[0m"
 
-first= yellow+'─'+reset #─ mangaSite.com
-middle=yellow+'├'+reset #  ├ chapter 1
-end=  yellow+"└"+reset #  │ └ part 1
-tree=  yellow+"│"+reset
+first='─' 
+middle='├' 
+end="└"  
+tree="│"
 
-upcolcurs="\033[{}A\033[{}G" #up,col
-downcolcurs="\033[{}B\033[{}G" #down,col
-colcurs="\033[{}G"
-clrtoeol="\033[0K"
+colcurs="\033[{}G" #go to column {}-1
+clrtoeol="\033[0K" #clear to end of line
 bars=["","▏","▎","▍","▌","▋","▊","▉","█"]
 
-def loadingBar(width,percentage):
+def loadingBar(width,percentage): #make a bar of width width, percentage% filled
 	units=(width*8)*(percentage/100)
 	if units>width*8:
 		units=width*8
 	return ((bars[8]*int(units/8))+bars[int(units%8)]).ljust(width," ")
 
-prefixes=[]
+prefixes=[] #store existing branches
 def node(key,data="",bracketed="",last=False):
 	global prefixes
-	if len(prefixes)==0:
-		output=first
+	if len(prefixes)==0: 
+		output=first #if there are no existing branches, the prefix is root
 	else:
-		output=yellow+"".join(prefixes[:-1])+"  "+reset
-		if last:
+		output=yellow+"".join(prefixes[:-1])+"  " #if there are existing branches, add all branches but present
+		if last: #add present branch according to context
 			output+=end
 		else:
 			output+=middle
-	output+=" "
-	if data=='\n' or data=='':
-		output+=blue
-	else:
+	output+=reset+" " #add padding for data
+	if data=='\n' or data=='': #if there is data, set to blue. 
+		output+=blue 
+	else: #else, set to cyan
 		output+=cyan
-	output+=key
-	if bracketed!="":
+	output+=key #add key, that can also be data if "data"==''
+	if bracketed!="": #add bracketed data beside key
 		output+=green+" ("+bracketed+")"
-	if data=="\n":
+	if data=="\n": #if data='\n', make colon yellow to signify nesting
 		output+=yellow+":"
-	elif data!="":
-		output+=cyan+": "+blue+data+reset
+	elif data!="": #if not nesting and data present, add data after colon and key
+		output+=cyan+": "+blue+data+reset 
 	print(output)
 
-	if last and not data=='\n':
+	if last and not data=='\n': #if need last node and not nesting, remove present branch and descend to most recent branch
 		prefixes.pop(-1)
 		for prefix in reversed(range(len(prefixes))):
 			if prefixes[prefix]=="   ":
 				prefixes.pop(prefix)
 			else:
 				break
-	if data=='\n':
-		if last:
+	if data=='\n': #if going down one level, add own branch
+		if last: #if last node, remove previous branch
 			prefixes[-1]="   "
 		prefixes+=["  │"]
